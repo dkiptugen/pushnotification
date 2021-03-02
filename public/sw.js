053366b1196f9d78924d27678009f2afbb58031d
Isaac Kiplel <38281391+Izooo@users.noreply.github.com>
2021-03-02 19:46:31 +0300

    changes

--- public/sw.js
+++ public/sw.js
@@ -33,3 +33,29 @@ self.addEventListener('push', function (e) {
         }));
     }
 });
+
+
+addEventListener('fetch', event => {
+    event.waitUntil(async function() {
+      // Exit early if we don't have access to the client.
+      // Eg, if it's cross-origin.
+      if (!event.clientId) return;
+  
+      // Get the client.
+      const client = await clients.get(event.clientId);
+      // Exit early if we don't get the client.
+      // Eg, if it closed.
+      if (!client) return;
+  
+      // Send a message to the client.
+      client.postMessage({
+        msg: "Hey I just got a fetch from you!",
+        url: event.request.url
+      });
+  
+    }());
+  });
+
+  navigator.serviceWorker.addEventListener('message', event => {
+    console.log(event.data.msg, event.data.url);
+  });
