fd0baf73449a56e5803f5e75f9bffe3c98b1741f
Isaac Kiplel <38281391+Izooo@users.noreply.github.com>
2021-04-14 11:22:24 +0300

    Login changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
-        'password',
+        'email'
     ];
 
     /**
@@ -29,7 +28,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password',
         'remember_token',
     ];
 
