e6fa44f52217fe5c904fee59676c3c1baf07cfa2
King Of The East <dennis.kiptoo@programmer.net>
2021-08-01 21:54:30 +0300

    furnish

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -19,43 +19,35 @@
 @include('includes.sidebar')
 <div class="main">
     <nav class="navbar navbar-expand navbar-light bg-white sticky-top">
-        <a class="sidebar-toggle d-flex mr-3 text-dark">
-            <i class="align-self-center" data-feather="menu"></i>
+        <a class="sidebar-toggle d-flex mr-3">
+            <i class="fas fa-bars fa-lg"></i>
         </a>
 
 
 
         <div class="navbar-collapse collapse">
-            <ul class="navbar-nav ml-auto mr-2">
-                <!-- Authentication Links -->
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-icon dropdown-toggle ml-2 d-inline-block d-sm-none" href="#" id="userDropdown" data-toggle="dropdown">
+                        <div class="position-relative">
+                            <i class="align-middle mt-n1" data-feather="settings"></i>
                         </div>
-                    </li>
-                @endguest
+                    </a>
+                    <a class="nav-link nav-link-user dropdown-toggle d-none d-sm-inline-block" href="#" id="userDropdown" data-toggle="dropdown">
+                        <img src="{{ asset('assets/img/avatar.png') }}" class="avatar img-fluid rounded-circle mr-1" alt="Avatar " />
+                        <span class="text-dark">{{ Auth::user()->name }}</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="">Profile</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}"  onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">{{ __('Sign out') }}</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+
+
 
             </ul>
         </div>
