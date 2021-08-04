e65a80c9eb1d36b5a7dc2fec803583888c4d9e99
Dennis Kiptoo <dennis@kenyans.co.ke>
2021-08-04 09:53:16 +0300

    sw

--- public/assets/js/enable-push.js
+++ public/assets/js/enable-push.js
@@ -16,7 +16,7 @@ function initSW() {
 
     //register the service worker
     navigator.serviceWorker.register('/assets/js/sw.js', {
-        scope: '/',
+        scope: '/assets/js/sw.js',
     }).then(function(registration) {
             console.log('serviceWorker installed!', registration.scope)
             initPush();
