a141e5532ad865cbc342fa89f701c2d679043704
Dennis Kiptoo <dennis@kenyans.co.ke>
2021-08-04 14:45:47 +0300

    sw

--- public/assets/js/enable-push.js
+++ public/assets/js/enable-push.js
@@ -70,23 +70,10 @@ function subscribeUser() {
             };
             console.log(subscribeOptions);
 
-            registration.pushManager.subscribe(subscribeOptions).then(
-                function(pushSubscription) {
-                    console.log(pushSubscription.endpoint);
-                    // The push subscription details needed by the application
-                    // server are now available, and can be sent to it using,
-                    // for example, an XMLHttpRequest.
-                }, function(error) {
-                    // During development it often helps to log errors to the
-                    // console. In a production environment it might make sense to
-                    // also report information about errors back to the
-                    // application server.
-                    console.log(error);
-                }
-            );
+            return registration.pushManager.subscribe(subscribeOptions);
         })
-        .then((pushSubscription) => {
-            console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
+        .then(function(pushSubscription){
+            //console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
             storePushSubscription(pushSubscription);
         });
 }
