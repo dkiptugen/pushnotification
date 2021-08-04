09b1b7a10fde2e4c8492adc0fea38f9b4078ae8e
Dennis Kiptoo <dennis@kenyans.co.ke>
2021-08-04 12:51:54 +0300

    sw

--- public/assets/js/enable-push.js
+++ public/assets/js/enable-push.js
@@ -41,24 +41,16 @@ function initSW() {
             console.log(err)
         });
 }
-
-if (Notification.permission === 'granted') {
-    subscribeUser();
-}
-else if (Notification.permission === 'default') {
-    $('#allow-push-notification-bar').show();
-}
-
-$('#allow-push-notification').click(function () {
-    $('#allow-push-notification-bar').hide();
+document.addEventListener('click', () => {
     Notification.requestPermission().then(function (status) {
         if (status === 'denied') {
-            //do something
+            //
         } else if (status === 'granted') {
             subscribeUser();
         }
     });
-});
+})
+
 function initPush() {
     if (!navigator.serviceWorker.ready) {
         return;
