ca978030183bd6f1f7160b2757ba8961f6b45b3a
Dennis Kiptoo <dennis@kenyans.co.ke>
2021-08-04 11:45:58 +0300

    sw

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
 //Route::middleware(['domain'])->group(function(){
     //Subscribe a user
     Route::post('/subscribe', [App\Http\Controllers\NotificationController::class, 'subscribe']);
