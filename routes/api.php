0ad7e926d2c62acce679bbfd046791469ba762d1
Dennis Kiptoo <dennis@kenyans.co.ke>
2021-08-04 14:27:09 +0300

    sw

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware(['domain'])->group(function(){
+Route::group(['middleware'=>['domain']],function(){
     //Subscribe a user
     Route::post('/subscribe', [App\Http\Controllers\NotificationController::class, 'subscribe']);
     //make a push notification.
