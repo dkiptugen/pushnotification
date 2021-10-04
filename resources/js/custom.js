9f258e37495fb821b117f78f89a3b3fbdb7475a8
Dennis Kiptugen <dennis@kenyans.co.ke>
2021-10-04 14:06:28 +0300

    icons

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -108,7 +108,7 @@ $(document).ready(function(){
             console.log('found');
         }
 
-        if(len < 160)
+        if(len <= 160)
             {
                 $('.msg-error').html((160-len)+" Charaters remaining");
             }
