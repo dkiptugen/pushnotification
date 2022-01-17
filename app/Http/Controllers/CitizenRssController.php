a9816c6f798ad57af917041d7b3db7b434c917f3
Dennis Kiptugen <dennis.kiptoo@boxraft.com>
2022-01-17 16:49:07 +0300

    publish draft

--- app/Http/Controllers/CitizenRssController.php
+++ app/Http/Controllers/CitizenRssController.php
@@ -52,7 +52,7 @@ class CitizenRssController extends Controller
             }
         public function index()
             {
-                dd($this->feed()->flatten(1));
+                dd($this->feed()->flatten(2));
 
                 $xml = "<rss version='2.0'>" . PHP_EOL;
                 $xml .= "<channel>" .PHP_EOL;
