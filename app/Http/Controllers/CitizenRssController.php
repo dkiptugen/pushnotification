c3e7b4716242cf29bdb4f875d5e4ecab91d59c4b
Dennis Kiptugen <dennis.kiptoo@boxraft.com>
2022-01-17 16:18:48 +0300

    publish draft

--- app/Http/Controllers/CitizenRssController.php
+++ app/Http/Controllers/CitizenRssController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Info;
+use Goutte\Client;
 use http\Env\Response;
 use Illuminate\Http\Request;
 
@@ -21,17 +22,50 @@ class CitizenRssController extends Controller
                 $xml .= "<title>Citizen.digital | RSS</title>" . PHP_EOL;
                 $xml .= "<description>Citizen Digital feed RSS</description>";
                 $xml .= "<language>en-us</language>" . PHP_EOL;
-                foreach(Info::orderBy('time','desc')->get() as $value)
+                $xml = simplexml_load_string(file_get_contents('https://citizen.digital/sitemap.xml'));
+                foreach($xml as $value)
                     {
-                        $xml .= "<item xmlns:dc='ns:1'>" . PHP_EOL;
-                        $xml .= "<title><![CDATA[".$value->title."]]></title>" . PHP_EOL;
-                        $xml .= "<description><![CDATA[".$value->caption."]]></description>" . PHP_EOL;
-                        $xml .= "<link>".$value->link."</link>" . PHP_EOL;
-                        $xml .= "<guid>".md5(rand(0,1000))."</guid>" . PHP_EOL;
-                        $xml .= "<pubDate>".$this->date_cleaner($value->time)."</pubDate>" . PHP_EOL;
-                        $xml .= "<dc:creator>".$value->author."</dc:creator>" . PHP_EOL;
-                        $xml .= "</item>" .PHP_EOL;
+                        preg_match('/\-n[0-9]+/',$value->loc,$match);
+                        $d=[];
+                        if(isset($match[0]))
+                            {
+                                $client = new Client();
+                                $d  =   [];
+                                $crawler = $client->request('GET', $value->loc);
+
+                                $d['content'] = $crawler->filter('.the-content ')->each(function ($node)
+                                {
+                                    return strip_tags($node->html(),'<p><br><h4><h3><h1><h2><h5><h6><a>');
+                                });
+                                $d['title'] = $crawler->filter('h2.page-title ')->each(function ($node)
+                                {
+                                    return $node->text();
+                                });
+                                $d['author'] = $crawler->filter('.authorinfo a')->each(function ($node)
+                                {
+                                    return $node->text();
+                                });
+                                $d['time'] = $crawler->filter('.datepublished')->each(function ($node)
+                                {
+                                    return date("D, d M Y H:i:s T", strtotime(str_replace('Published on: ','',$node->text())));
+                                });
+
+                                $xml .= "<item xmlns:dc='ns:1'>" . PHP_EOL;
+                                $xml .= "<title><![CDATA[".$d['title']."]]></title>" . PHP_EOL;
+                                $xml .= "<description><![CDATA[".$d['content']."]]></description>" . PHP_EOL;
+                                $xml .= "<link>".$value->loc."</link>" . PHP_EOL;
+                                $xml .= "<guid>".md5(rand(0,1000))."</guid>" . PHP_EOL;
+                                $xml .= "<pubDate>".$d['time']."</pubDate>" . PHP_EOL;
+                                $xml .= "<dc:creator>".$d['author']."</dc:creator>" . PHP_EOL;
+                                $xml .= "</item>" .PHP_EOL;
+                            }
+
+
+
                     }
+
+
+
                 $xml .= "</channel>".PHP_EOL;
                 $xml .= "</rss>".PHP_EOL;
                 return response($xml, 200)
