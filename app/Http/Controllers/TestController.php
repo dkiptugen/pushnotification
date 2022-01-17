2d4084be2bdfa98eaf2fd63c5d1214e606e3ef3c
Dennis Kiptugen <dennis.kiptoo@boxraft.com>
2022-01-17 15:47:49 +0300

    publish draft

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -14,6 +14,7 @@ class TestController extends Controller
                foreach($xml as $value)
                    {
                        preg_match('/\-n[0-9]+/',$value->loc,$match);
+                       $d=[];
                        if(isset($match[0]))
                            {
                                $client = new Client();
@@ -22,8 +23,9 @@ class TestController extends Controller
 
                                 $crawler->filter('.the-content ')->each(function ($node)
                                    {
-                                         dump(strip_tags($node->html(),'<p><br><h4><h3><h1><h2><h5><h6><a>'));
+                                         $d['content'] = strip_tags($node->html(),'<p><br><h4><h3><h1><h2><h5><h6><a>');
                                    });
+                                dump($d);
                            }
 
                             echo $value->loc.' : '.(int)str_replace('-n','',$match[0]).'</br>';
