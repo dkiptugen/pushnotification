0ccb496061deb6217eb070743cc79657faf23176
Dennis Kiptugen <dennis.kiptoo@boxraft.com>
2022-01-17 17:15:16 +0300

    publish draft

--- app/Http/Controllers/CitizenRssController.php
+++ app/Http/Controllers/CitizenRssController.php
@@ -15,7 +15,7 @@ class CitizenRssController extends Controller
                 $d= [];
                 $xmlx = simplexml_load_string(file_get_contents('https://citizen.digital/sitemap.xml'));
 
-                $xml = collect($xmlx)->chunk(30);
+                $xml = array_chunk((array)$xmlx,30)
                 dd($xml);
                     $x = 0;
                     foreach($xml as $value) {
