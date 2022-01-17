392b27ca36e89353ed6c461016bc980d47febf26
Dennis Kiptugen <dennis.kiptoo@boxraft.com>
2022-01-17 14:33:02 +0300

    publish draft

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -16,11 +16,12 @@ class TestController extends Controller
                        preg_match('/\-n[0-9]+/',$value->loc,$match);
                        if(isset($match[0]))
                            {
-                               $fdff = simplexml_load_string(file_get_contents('https://citizen.digital/sitemap.xml'));
+                               $fdff = simplexml_load_string(file_get_contents($value->loc));
                                dd($fdff);
                            }
 
                             echo $value->loc.' : '.(int)str_replace('-n','',$match[0]).'</br>';
+                       die();
                    }
             }
     }
