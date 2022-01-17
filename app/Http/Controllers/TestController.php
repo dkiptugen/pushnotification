ed0a96e05be4e44dbb5f6bb70c50068f5b68f794
Dennis Kiptugen <dennis.kiptoo@boxraft.com>
2022-01-17 14:02:11 +0300

    publish draft

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Utils\Sunra\HtmlDomParser;
 use App\Utils\TelegramPost;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,15 @@ class TestController extends Controller
                    {
                        preg_match('/\-n[0-9]+/',$value->loc,$match);
                        if(isset($match[0]))
-                            echo $value->loc.' : '.(int)str_replace('-n','',$match[0]).'</br>';
+                           {
+                               $content = HtmlDomParser::file_get_html($value->loc);
+                               foreach($content->find('div.article-content') as $val)
+                                   {
+                                       $details["title"]			= 	strip_tags($val->getElementByTagName("h2")->innertext);
+                                   }
+                           }
+
+                            echo $value->loc.' : '.(int)str_replace('-n','',$match[0]).' - '.$details["title"].'</br>';
                    }
             }
     }
