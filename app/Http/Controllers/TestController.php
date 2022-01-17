dd231458a482547bde8bc19b5820a58a6c12b4ba
Dennis Kiptugen <dennis.kiptoo@boxraft.com>
2022-01-17 14:50:49 +0300

    publish draft

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -22,7 +22,7 @@ class TestController extends Controller
 
                                 $crawler->filter('div.article-content')->each(function ($node)
                                    {
-                                       print  $node->filter('h2.pafe_title').text();
+                                       print  $node->filter('h2.page_title').text();
                                    });
                            }
 
