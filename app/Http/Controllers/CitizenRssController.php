22549e182597fccb133da071cfab55bf4f6a6eb8
Dennis Kiptugen <dennis.kiptoo@boxraft.com>
2022-01-17 17:04:22 +0300

    publish draft

--- app/Http/Controllers/CitizenRssController.php
+++ app/Http/Controllers/CitizenRssController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\ProcessFeed;
 use App\Models\Info;
 use Goutte\Client;
 use http\Env\Response;
@@ -12,47 +13,42 @@ class CitizenRssController extends Controller
         public function feed()
             {
                 $d= [];
-                $xml = simplexml_load_string(file_get_contents('https://citizen.digital/sitemap.xml'));
-                $x=0;
-                foreach($xml as $value)
-                    {
-                        preg_match('/\-n[0-9]+/',$value->loc,$match);
+                $xmlx = simplexml_load_string(file_get_contents('https://citizen.digital/sitemap.xml'));
+                return $xmlx->chunk(2,function ($xml){
+                    $x = 0;
+                    foreach($xml as $value) {
+                        preg_match('/\-n[0-9]+/', $value->loc, $match);
 
-                        if(isset($match[0]))
-                            {
-                                $client = new Client();
-                                $crawler = $client->request('GET', $value->loc);
+                        if (isset($match[0])) {
+                            $client = new Client();
+                            $crawler = $client->request('GET', $value->loc);
 
-                                $d[$x]['content'] = $crawler->filter('.the-content ')->each(function ($node)
-                                {
-                                    return strip_tags($node->html(),'<p><br><h4><h3><h1><h2><h5><h6><a>');
-                                });
-                                $d[$x]['title'] = $crawler->filter('h2.page-title ')->each(function ($node)
-                                {
-                                    return $node->text();
-                                });
-                                $d[$x]['author'] = $crawler->filter('.authorinfo a')->each(function ($node)
-                                {
-                                    return $node->text();
-                                });
-                                $d[$x]['time'] = $crawler->filter('.datepublished')->each(function ($node)
-                                {
-                                    return date("D, d M Y H:i:s T", strtotime(str_replace('Published on: ','',$node->text())));
-                                });
-                                $d[$x]['id']  =   (int)str_replace('-n','',$match[0]);
-                                $d[$x]['link']    =    (string)$value->loc;
-                                $x++;
-                            }
-                        if($x==2)
-                            break;
+                            $d[$x]['content'] = $crawler->filter('.the-content ')->each(function ($node) {
+                                return strip_tags($node->html(), '<p><br><h4><h3><h1><h2><h5><h6><a>');
+                            });
+                            $d[$x]['title'] = $crawler->filter('h2.page-title ')->each(function ($node) {
+                                return $node->text();
+                            });
+                            $d[$x]['author'] = $crawler->filter('.authorinfo a')->each(function ($node) {
+                                return $node->text();
+                            });
+                            $d[$x]['time'] = $crawler->filter('.datepublished')->each(function ($node) {
+                                return date("D, d M Y H:i:s T", strtotime(str_replace('Published on: ', '', $node->text())));
+                            });
+                            $d[$x]['id'] = (int)str_replace('-n', '', $match[0]);
+                            $d[$x]['link'] = (string)$value->loc;
+                            $x++;
+                        }
 
                     }
-                return collect($d);
+                    return collect($d);
+                });
+
 
             }
         public function index()
             {
-                dd($this->feed()->flatten(3));
+                dd($this->feed());
 
                 $xml = "<rss version='2.0'>" . PHP_EOL;
                 $xml .= "<channel>" .PHP_EOL;
