6dd4f3e052ef8f54a11d568416bd525433735084
Dennis Kiptugen <dennis.kiptoo@boxraft.com>
2022-02-01 10:09:04 +0300

    homepage

--- resources/views/modules/dashboard/index.blade.php
+++ resources/views/modules/dashboard/index.blade.php
@@ -2,7 +2,10 @@
 @section('content')
     <div class="d-flex justify-content-end w-100">
         <div class="ml-auto">
-            <input class="form-control" type="text" name="daterange" value="01/01/2018 - 01/15/2018" id="reportrange" />
+            <form action="" class="form" method="get">
+                <input class="form-control" type="text" name="daterange" value="01/01/2018 - 01/15/2018" id="reportrange" />
+            </form>
+
         </div>
     </div>
     <div class="row mt-2">
